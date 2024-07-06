80519227a425b0f0d74c5aaac71a405af8e38676
Hasinarahmani <107977511+Hasinarahmani@users.noreply.github.com>
2024-07-06 16:44:59 +0430

    adding eventlistener

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-const input = document.getElementById("user-input");
-const check = document.getElementById("check-btn");
-const clear = document.getElementById("clear-btn");
-const results = document.getElementById("results-div");
+const input = document.getElementById('user-input');
+const check = document.getElementById('check-btn');
+const clear = document.getElementById('clear-btn');
+const results = document.getElementById('results-div');
 
-check.addEventListener("click", () => {
-const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/
+check.addEventListener('click', () => {
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
 
   if (!input.value) {
-    alert("Please provide a phone number")
-  } else if (regex.test(input.value)){
-results.innerText = `Valid US number: ${input.value}`
+    alert('Please provide a phone number');
+  } else if (regex.test(input.value)) {
+    results.innerText = `Valid US number: ${input.value}`
   } else {
-    results.innerText = `InValid US number: ${input.value}`
+    results.innerText = `InValid US number: ${input.value}`;
   }
 });
 
-clear.addEventListener("click", () => results.innerText = "")
-
-
-
+clear.addEventListener('click', () => results.innerText = '');
